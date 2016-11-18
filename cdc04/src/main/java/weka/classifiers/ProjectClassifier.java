124fc1a44b66bc13c8183efecfcb86fbe34623b8
Chris McKee <cmckee41@qub.ac.uk>
2016-11-18 01:20:49 +0000

    Added some test files, played with classifiers
    Turns out, you can specify the attribute that you want to classify. However, it doesn't look like you can classify a whole data set in a batch at the same time. Seems like you have to do it one instance at a time, which will prove messy.
    
    One day I'll get my gitignore file right

--- cdc04/src/main/java/weka/classifiers/ProjectClassifier.java
+++ cdc04/src/main/java/weka/classifiers/ProjectClassifier.java
@@ -3,6 +3,11 @@ package weka.classifiers;
 import weka.core.Capabilities;
 import weka.core.Instance;
 import weka.core.Instances;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashMap;
+
 import weka.classifiers.AbstractClassifier;
 import weka.classifiers.Classifier;
 import weka.classifiers.trees.J48;
@@ -11,7 +16,7 @@ public class ProjectClassifier extends AbstractClassifier {
 
 	private static final long serialVersionUID = 3582366333379609425L;
 	
-	private Classifier tester = new J48();
+	private ArrayList<Classifier> classifiers;
 
 	public String globalInfo() {
 		return "Repeatedly applies, then rebuilds models until the output data set no longer changes.";
@@ -21,7 +26,13 @@ public class ProjectClassifier extends AbstractClassifier {
 	@Override
 	public void buildClassifier(Instances data) throws Exception {
 		// TODO Auto-generated method stub
-		
+		findMissingAttributes(data);
+		for (int j = 0; j < data.numAttributes(); j++) {
+			Classifier tester = new J48();
+			data.setClassIndex(j);
+			tester.buildClassifier(data);
+			classifiers.set(j, tester);
+		}
 	}
 
 	@Override
@@ -40,6 +51,26 @@ public class ProjectClassifier extends AbstractClassifier {
 	public Capabilities getCapabilities() {
 		// TODO Auto-generated method stub
 		return null;
+	}
+	
+	private HashMap<Integer, Collection<Integer>> findMissingAttributes(Instances instances) {
+		HashMap<Integer, Collection<Integer>> missing = new HashMap<Integer, Collection<Integer>>();
+		// Initialise a list of instances with missing attributes for every column
+		for (int i=0; i < instances.numAttributes(); i++){
+			missing.put(i, new ArrayList<Integer>());
+		}
+		
+		for (int i = 0; i < instances.numInstances(); i++) {
+			Instance test = instances.get(i);
+			for (int j = 0; j < test.numAttributes(); j++) {
+				// Add the index of the instance which contains the attribute to that particular
+				// attribute's list
+				if (test.isMissing(j)) {
+					missing.get(j).add(i);
+				}
+			}
+		}
+		return missing;
 	}
 
 	public static void main (String [] args) {
--- cdc04/src/main/java/weka/filters/unsupervised/attribute/ATestFilter.java
+++ cdc04/src/main/java/weka/filters/unsupervised/attribute/ATestFilter.java
@@ -6,9 +6,9 @@ import java.util.HashMap;
 
 import weka.core.Instance;
 import weka.core.Instances;
-import weka.filters.SimpleStreamFilter;
+import weka.filters.SimpleBatchFilter;
 
-public class ATestFilter extends SimpleStreamFilter {
+public class ATestFilter extends SimpleBatchFilter {
 
 	/**
 	 * 
@@ -17,7 +17,7 @@ public class ATestFilter extends SimpleStreamFilter {
 
 	@Override
 	public String globalInfo() {
-		return "Fires in a load of missing values for the wee thing lad";
+		return "Test filter which does nothing";
 	}
 
 	@Override
@@ -28,7 +28,7 @@ public class ATestFilter extends SimpleStreamFilter {
 	@Override
 	protected Instances process(Instances instances) throws Exception {
 		HashMap<Integer, Collection<Integer>> missing = findMissingAttributes(instances);
-		System.out.println(missing);
+		System.out.println(instances.classAttribute());
 		return instances;
 	}
 
@@ -36,13 +36,6 @@ public class ATestFilter extends SimpleStreamFilter {
 	protected Instances determineOutputFormat(Instances inputFormat) throws Exception {
 		// TODO Auto-generated method stub
 		return null;
-	}
-
-	@Override
-	protected Instance process(Instance instance) throws Exception {
-		System.out.println("CATS");
-		// System.out.println(instance.enumerateAttributes());
-		return instance;
 	}
 
 	private HashMap<Integer, Collection<Integer>> findMissingAttributes(Instances instances) {
