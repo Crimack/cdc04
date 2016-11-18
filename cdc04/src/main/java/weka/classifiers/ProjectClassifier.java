de5a5010387c73aebcf783f07c719c2cf53369d3
Christopher McKee <cmckee41@qub.ac.uk>
2016-11-18 20:40:27 +0000

    Wrote a basic implementation of project. Ignores training sets
    This needs some work. It runs as a filter, and also only runs in a
    very specific way. On the plus side, it's currently top of the board
    for the AI3 challenge

--- cdc04/src/main/java/weka/classifiers/ProjectClassifier.java
+++ cdc04/src/main/java/weka/classifiers/ProjectClassifier.java
@@ -1,80 +1,121 @@
 package weka.classifiers;
 
+import weka.classifiers.trees.J48;
 import weka.core.Capabilities;
 import weka.core.Instance;
 import weka.core.Instances;
 
-import java.util.ArrayList;
 import java.util.Collection;
 import java.util.HashMap;
-
-import weka.classifiers.AbstractClassifier;
-import weka.classifiers.Classifier;
-import weka.classifiers.trees.J48;
+import java.util.Iterator;
+import java.util.LinkedList;
 
 public class ProjectClassifier extends AbstractClassifier {
 
-	private static final long serialVersionUID = 3582366333379609425L;
-	
-	private ArrayList<Classifier> classifiers;
+    private static final long serialVersionUID = 3582366333379609425L;
 
-	public String globalInfo() {
-		return "Repeatedly applies, then rebuilds models until the output data set no longer changes.";
-		
-	}
-	
-	@Override
-	public void buildClassifier(Instances data) throws Exception {
-		// TODO Auto-generated method stub
-		findMissingAttributes(data);
-		for (int j = 0; j < data.numAttributes(); j++) {
-			Classifier tester = new J48();
-			data.setClassIndex(j);
-			tester.buildClassifier(data);
-			classifiers.set(j, tester);
-		}
-	}
+    // Classification variables
+    private Classifier[] classifiers;
+    private HashMap<Integer, Collection<Integer>> missing;
 
-	@Override
-	public double classifyInstance(Instance instance) throws Exception {
-		// TODO Auto-generated method stub
-		return 0;
-	}
+    // Instances used to track progress
+    private Instances original;
+    private Instances last;
+    private Instances current;
 
-	@Override
-	public double[] distributionForInstance(Instance instance) throws Exception {
-		// TODO Auto-generated method stub
-		return null;
-	}
+    public String globalInfo() {
+        return "Repeatedly applies, then rebuilds models until the output data set no longer changes.";
 
-	@Override
-	public Capabilities getCapabilities() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-	
-	private HashMap<Integer, Collection<Integer>> findMissingAttributes(Instances instances) {
-		HashMap<Integer, Collection<Integer>> missing = new HashMap<Integer, Collection<Integer>>();
-		// Initialise a list of instances with missing attributes for every column
-		for (int i=0; i < instances.numAttributes(); i++){
-			missing.put(i, new ArrayList<Integer>());
-		}
-		
-		for (int i = 0; i < instances.numInstances(); i++) {
-			Instance test = instances.get(i);
-			for (int j = 0; j < test.numAttributes(); j++) {
-				// Add the index of the instance which contains the attribute to that particular
-				// attribute's list
-				if (test.isMissing(j)) {
-					missing.get(j).add(i);
-				}
-			}
-		}
-		return missing;
-	}
+    }
 
-	public static void main (String [] args) {
-		runClassifier(new ProjectClassifier(), args);
-	}
+    @Override
+    public void buildClassifier(Instances data) throws Exception {
+        findMissingAttributes(data);
+        original = new Instances(data);
+        // Force instantiation as a quick hack to remove null errors.
+        current = new Instances(data);
+        last = new Instances(data);
+        last.remove(0);
+
+
+        trainClassifiers(data);
+    }
+
+    private void trainClassifiers(Instances data) throws Exception {
+        classifiers = new Classifier[data.numAttributes()];
+        for (int j = 0; j < data.numAttributes(); j++) {
+            Classifier tester = new J48();
+            data.setClassIndex(j);
+            tester.buildClassifier(data);
+            classifiers[j] = tester;
+        }
+    }
+
+    @Override
+    public double classifyInstance(Instance instance) throws Exception {
+        /**
+         * Should only be run through classifyDataset
+         */
+        Classifier active = classifiers[instance.classIndex()];
+        return active.classifyInstance(instance);
+    }
+
+    @Override
+    public Capabilities getCapabilities() {
+        return null;
+    }
+
+    public Instances classifyDataset() throws Exception {
+        int counter = 1;
+        while (!current.toString().equals(last.toString())) {
+            System.out.println("Number of iterations: " + counter);
+            last = new Instances(current);
+            current = new Instances(original);
+            System.out.println(last.toString() == null);
+            System.out.println(current.toString() == null);
+            trainClassifiers(last);
+            for (int i=0; i < current.numAttributes(); i++) {
+                current.setClassIndex(i);
+                Iterator<Integer> instanceNumbers = missing.get(i).iterator();
+                while (instanceNumbers.hasNext()) {
+                    int index = instanceNumbers.next();
+                    Instance a = current.get(index);
+                    a.setValue(i, classifyInstance(a));
+
+                }
+            }
+            counter++;
+            System.out.println(last.toString() == null);
+            System.out.println(current.toString() == null);
+            System.out.println("Lets go for another round");
+            System.out.println(current.toString().equals(last.toString()));
+            System.out.println();
+        }
+        return current;
+    }
+
+    private HashMap<Integer, Collection<Integer>> findMissingAttributes(Instances instances) {
+        missing = new HashMap<Integer, Collection<Integer>>();
+        // Initialise a list of instances with missing attributes for every column
+        for (int i = 0; i < instances.numAttributes(); i++) {
+            missing.put(i, new LinkedList<Integer>());
+        }
+        for (int i = 0; i < instances.numInstances(); i++) {
+            Instance test = instances.get(i);
+            for (int j = 0; j < test.numAttributes(); j++) {
+                // Add the index of the instance which contains the attribute to that particular
+                // attribute's list
+                if (test.isMissing(j)) {
+                    missing.get(j).add(i);
+                }
+            }
+        }
+        return missing;
+    }
+
+
+    public static void main(String[] args) {
+        runClassifier(new ProjectClassifier(), args);
+    }
 
 }
--- cdc04/src/main/java/weka/filters/unsupervised/attribute/ATestFilter.java
+++ cdc04/src/main/java/weka/filters/unsupervised/attribute/ATestFilter.java
@@ -1,10 +1,6 @@
 package weka.filters.unsupervised.attribute;
 
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.HashMap;
-
-import weka.core.Instance;
+import weka.classifiers.ProjectClassifier;
 import weka.core.Instances;
 import weka.filters.SimpleBatchFilter;
 
@@ -27,35 +23,17 @@ public class ATestFilter extends SimpleBatchFilter {
 
 	@Override
 	protected Instances process(Instances instances) throws Exception {
-		HashMap<Integer, Collection<Integer>> missing = findMissingAttributes(instances);
 		System.out.println(instances.classAttribute());
-		return instances;
+		ProjectClassifier a = new ProjectClassifier();
+		a.buildClassifier(instances);
+		Instances answer = a.classifyDataset();
+		System.out.println(answer.toString());
+		return answer;
 	}
 
 	@Override
 	protected Instances determineOutputFormat(Instances inputFormat) throws Exception {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	private HashMap<Integer, Collection<Integer>> findMissingAttributes(Instances instances) {
-		HashMap<Integer, Collection<Integer>> missing = new HashMap<Integer, Collection<Integer>>();
-		// Initialise a list of instances with missing attributes for every column
-		for (int i=0; i < instances.numAttributes(); i++){
-			missing.put(i, new ArrayList<Integer>());
-		}
-		
-		for (int i = 0; i < instances.numInstances(); i++) {
-			Instance test = instances.get(i);
-			for (int j = 0; j < test.numAttributes(); j++) {
-				// Add the index of the instance which contains the attribute to that particular
-				// attribute's list
-				if (test.isMissing(j)) {
-					missing.get(j).add(i);
-				}
-			}
-		}
-		return missing;
+		return inputFormat;
 	}
 
 }
