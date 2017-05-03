61910c86c61861d016d7aa5fc330877f9ea40bf0
Chris McKee <cmckee41@qub.ac.uk>
2017-05-03 19:21:54 +0100

    Added rest of tables for hidden variables appendix
    Also shrunk the margins very slightly, changed spacing scheme,
    and made tables less ridiculous throughout

--- doc/dissertation/McKee/spellcheck.sh
+++ doc/dissertation/McKee/spellcheck.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-TARGET_FILES="dissertation.tex acknowledgements.tex abstract.tex intro.tex requirements.tex design_implementation.tex experiments.tex conclusion.tex datasets.tex hidden_variables.tex user_guide.tex"
+TARGET_FILES="dissertation.tex acknowledgements.tex abstract.tex intro.tex requirements.tex design_implementation.tex experiments.tex conclusion.tex datasets.tex results.tex hidden_variables.tex user_guide.tex"
 
 for f in $TARGET_FILES
 do
