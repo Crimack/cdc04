d436e3613fd6a0fa3df47de49c7c299148ba44f9
Chris McKee <cmckee41@qub.ac.uk>
2017-05-10 15:23:31 +0100

    Edited scripts to work
    Ran a load of git filter-branch commands when preparing to upload to
    public repository, scripts were pointing at wrong directories etc

--- experiments/filter_arff.sh
+++ experiments/filter_arff.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
-# Bash script to remove percentage of values from all test files
-# Works if run from top level of repo and weka.jar is built
+# Bash script to remove percentage (10% by default) of values from all test files
+# Accepts weka.jar path as first arg, should be run from cdc04/experiments
 
-JAR_LOCATION="Code/Weka/dist/weka.jar"
-INPUT_FILES=`du Test\ Files/* | cut -f2`
+JAR_LOCATION=$1
+INPUT_FILES=`du test_files/* | cut -f2`
 
 SAVEIFS=$IFS
 IFS="$(echo -en "\n\b")"
 for f in $INPUT_FILES
 do
-save_location="Filtered "$f
+save_location="filtered_"$f
 java -classpath $JAR_LOCATION weka.filters.unsupervised.attribute.ReplaceWithMissingValue \
 -R first-last \
 -S 1 \
--- parse_arff_to_table.py
+++ parse_arff_to_table.py
@@ -2,10 +2,10 @@
 import os
 
 data = []
-target_path = os.path.join(os.getcwd(), "Test Files")
-for filename in os.listdir(target_path):
+TARGET_PATH = os.path.join(os.getcwd(), "experiments", "test_files")
+for filename in os.listdir(TARGET_PATH):
     if filename.endswith(".arff"):
-	    with open(os.path.join(target_path, filename)) as f:
+	    with open(os.path.join(TARGET_PATH, filename)) as f:
 		info = {"name": filename.replace("_", "-")[:-5], "num_atts": 0, "num_instances": 0}
 		for line in f.readlines():
 		    if line.startswith("@attribute"):
@@ -15,18 +15,16 @@ for filename in os.listdir(target_path):
 		data.append(info)
         
 
-output_path = os.path.join(os.getcwd(), "Docs", "Dissertation", "McKee", "datasets_table.tex")
-with open(output_path, "w") as g:
-    g.write("\\begin{table}[thb]\n")
+OUTPUT_PATH = os.path.join(os.getcwd(), "doc", "dissertation", "datasets_table.tex")
+with open(OUTPUT_PATH, "w") as g:
+    g.write("{\\centering \\footnotesize \\begin{longtable}{lrr@{\hspace{0.1cm}}cr@{\hspace{0.1cm}}c}\n")
     g.write("\caption{\label{dsets} Experimental Data Sets}\n")
-    g.write("\\footnotesize")
-    g.write("{\\centering \\begin{tabular}{lrr@{\hspace{0.1cm}}cr@{\hspace{0.1cm}}c}\\\\\n")
+    g.write("\\\\\n")
     g.write("\hline\\\\\n")
     g.write("Dataset Title & Num. Attributes & Num. Instances\\\\\n")
     g.write("\hline\\\\\n")
     for entry in sorted(data, key=lambda k: k["name"]):
         g.write("{name} & {num_atts} & {num_instances}\\\\\n".format(**entry))
     g.write("\hline\\\\\n")
-    g.write("\end{tabular} \\footnotesize \par}\n")
-    g.write("\end{table}")
-    
+    g.write("\end{longtable} \\footnotesize \par}\n")
+    g.write("\\newpage\n")
